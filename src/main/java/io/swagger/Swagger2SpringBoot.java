0baf904d49d89b2c92d80439c7c465124163f17e
vajadhav <vandana1_jadhav@yahoo.co.in>
2016-12-15 18:51:17 +0530

    fix for "Could not autowire field: org.springframework.web.client.RestTemplate...." issue

--- src/main/java/io/swagger/Swagger2SpringBoot.java
+++ src/main/java/io/swagger/Swagger2SpringBoot.java
@@ -5,11 +5,18 @@ import org.springframework.boot.ExitCodeGenerator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
 public class Swagger2SpringBoot implements CommandLineRunner {
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 
 	@Override
 	public void run(String... arg0) throws Exception {
